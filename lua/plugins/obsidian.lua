60eaecd2183b8209641b5a9cf8f7798afcccbcf3
daveM <david.meredith@stfc.ac.uk>
2024-02-06 15:25:08 +0000

    Bug fix obsidian conf: put custom conf within opts
    Nesting level wasn't correct, custom config needs to go within the opts
    block

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -33,23 +33,29 @@ return {
       -- A map for custom variables, the key should be the variable and the value a function
       substitutions = {},
     },
-  },
 
-  mappings = {
-    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault (default)
-    ["gf"] = {
-      action = function()
-        return require("obsidian").util.gf_passthrough()
-      end,
-      opts = { noremap = false, expr = true, buffer = true },
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault (default)
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes, changed to oh as '<leader>ch' clashes with code action
+      ["<leader>oh"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
     },
-    -- Toggle check-boxes, changed to oh as '<leader>ch' clashes with code action, but i can't
-    -- get this to work correctly
-    ["<leader>oh"] = {
-      action = function()
-        return require("obsidian").util.toggle_checkbox()
-      end,
-      opts = { buffer = true },
-    },
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+    -- URL it will be ignored but you can customize this behavior here.
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.fn.jobstart({ "open", url }) -- Mac OS
+      -- vim.fn.jobstart({"xdg-open", url})  -- linux
+    end,
   },
 }
