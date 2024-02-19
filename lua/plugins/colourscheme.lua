59f2b1ff216d24ae7708558b25109b36a31e3559
daveM <david.meredith@stfc.ac.uk>
2024-02-19 18:56:07 +0000

    Moving to catppuccin-frappe

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -33,47 +33,14 @@
 --}
 
 return {
-  "catppuccin/nvim",
-  lazy = true,
-  name = "catppuccin",
-  opts = {
-    integrations = {
-      aerial = true,
-      alpha = true,
-      cmp = true,
-      dashboard = true,
-      flash = true,
-      gitsigns = true,
-      headlines = true,
-      illuminate = true,
-      indent_blankline = { enabled = true },
-      leap = true,
-      lsp_trouble = true,
-      mason = true,
-      markdown = true,
-      mini = true,
-      native_lsp = {
-        enabled = true,
-        underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
-        },
-      },
-      navic = { enabled = true, custom_bg = "lualine" },
-      neotest = true,
-      neotree = true,
-      noice = true,
-      notify = true,
-      semantic_tokens = true,
-      telescope = true,
-      treesitter = true,
-      treesitter_context = true,
-      which_key = true,
+  {
+    "catppuccin",
+    opts = {
+      -- https://www.reddit.com/r/neovim/comments/18eobbj/transparency_catppuccin_with_lazyvim_not_working/
+      transparent_background = true,
+      flavour = "frappe",
     },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
@@ -81,3 +48,65 @@ return {
     },
   },
 }
+
+--return {
+--  "catppuccin/nvim",
+--  lazy = true,
+--  name = "catppuccin",
+--  -- Where to put catppuccin config is lazyvim
+--  -- https://www.reddit.com/r/neovim/comments/12eylee/where_to_put_catppuccin_configuration/
+--  -- config = function()
+--  --   require("catppuccin").setup({
+--  --     transparent_background = true,
+--  --     flavour = "latte",
+--  --   })
+--  --   --vim.cmd([[colorscheme catppuccin]])
+--  -- end,
+--  --
+--  opts = {
+--    flavour = "latte",
+--    transparent_background = true,
+--    integrations = {
+--      aerial = true,
+--      alpha = true,
+--      cmp = true,
+--      dashboard = true,
+--      flash = true,
+--      gitsigns = true,
+--      headlines = true,
+--      illuminate = true,
+--      indent_blankline = { enabled = true },
+--      leap = true,
+--      lsp_trouble = true,
+--      mason = true,
+--      markdown = true,
+--      mini = true,
+--      native_lsp = {
+--        enabled = true,
+--        underlines = {
+--          errors = { "undercurl" },
+--          hints = { "undercurl" },
+--          warnings = { "undercurl" },
+--          information = { "undercurl" },
+--        },
+--      },
+--      navic = { enabled = true, custom_bg = "lualine" },
+--      neotest = true,
+--      neotree = true,
+--      noice = true,
+--      notify = true,
+--      semantic_tokens = true,
+--      telescope = true,
+--      treesitter = true,
+--      treesitter_context = true,
+--      which_key = true,
+--    },
+--  },
+--
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "catppuccin",
+--    },
+--  },
+--}
