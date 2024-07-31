0885d0f1b75a0899115936db1d1aa30de2036c4b
daveM <david.meredith@stfc.ac.uk>
2024-07-31 18:47:14 +0100

    Remapping Flash Treesitter to Z to not clash with Surround S.

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -6,8 +6,8 @@ return {
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    -- DM: Changing default 'S' to ' S' (ie. <leader>S) so it does not clash with Surround.nvim.
-    { " S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- DM: Changing default 'S' to 'Z' so it does not clash with Surround.nvim (would have preferred "<leader>S" but can't get that to work.
+    { "Z", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
