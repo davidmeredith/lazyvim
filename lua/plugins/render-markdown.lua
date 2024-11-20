4b6b4631c8b821bf8fa87b716f953787241dce2a
daveM <david.meredith@stfc.ac.uk>
2024-11-20 11:34:24 +0000

    Snacks and Avanti

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -14,7 +14,7 @@ return {
   ft = { "markdown", "norg", "rmd", "org" },
   config = function(_, opts)
     require("render-markdown").setup(opts)
-    LazyVim.toggle.map("<leader>um", {
+    Snacks.toggle({
       name = "Render Markdown",
       get = function()
         return require("render-markdown.state").enabled
@@ -27,6 +27,6 @@ return {
           m.disable()
         end
       end,
-    })
+    }):map("<leader>um")
   end,
 }
